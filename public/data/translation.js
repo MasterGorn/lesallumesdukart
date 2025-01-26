357292c5fa9617dbd03d52ffdd813f9a1f798583
Pierre Brunet <brunet.pierre.gorn@gmail.com>
2025-01-26 16:35:07 +0100

    add 10 tips and style beta

---
+++ public/data/translation.js
@@ -0,0 +1,196 @@
+const translations = {
+    fr: {
+        subnav: {
+            maps: "Secrets des maps",
+            tips: "Astuces",
+            beta: "Beta"
+        },
+        legend: {
+            show: "Afficher la légende",
+            hide: "Masquer la légende",
+            difficulty: {
+                titleDifficulty: "Difficulté",
+                veryEasy: "Très facile",
+                easy: "Facile",
+                medium: "Moyen",
+                hard: "Difficile",
+                veryHard: "Très difficile"
+            },
+            gain: {
+                titleGain: "Gain",
+                veryLittle: "Une mini portion",
+                little: "⅛ de tour",
+                medium: "¼ de tour",
+                aLot: "½ tour",
+                reallyALot: "1 tour"
+            },
+            mode: {
+                titleMode: "Modes",
+                time: "Time Trial",
+                GrandPrix: "Grand Prix",
+                Multiplayers: "Multijoueurs",
+            }
+        },
+        size: {
+            decrease: "Réduire",
+            increase: "Agrandir"
+        },
+        circuitsData: circuitsDataFR,
+        tips: {
+            title: "Astuces",
+            tip1: {
+                title: "🏎️ Carapace rouge en première position",
+                content: "Une carapace rouge tirée en première position fera le tour du circuit jusqu’à toucher un joueur sur la route. 🎯"
+            },
+            tip2: {
+                title: "🌬️ Aspiration",
+                content: "Profitez de l’aspiration derrière un autre joueur pour gagner en vitesse et dépasser facilement. 🚀"
+            },
+            tip3: {
+                title: "🍌 Éviter de glisser sur une banane",
+                content: "Si vous touchez une banane, appuyez rapidement sur le bouton de frein 🅱️ pendant une secondepour ne pas perdre le contrôle de votre véhicule. Une petite note de musique 🎵 s’affichera si la manipulation est réussie !"
+            },
+            tip4: {
+                title: "🚦 Départ rapide",
+                content: "Appuyez sur le bouton d’accélération juste avant que la lumière devienne verte pour un départ boosté."
+            },
+            tip5: {
+                title: "🏎️ Accélération après un virage",
+                content: "Utilisez le bouton R pour déraper en sortie de virage. Bougez la manette de gauche à droite pour changer la fumée (jaune → orange). Relâchez R quand elle est orange pour une accélération."
+            },
+            tip6: {
+                title: "🛡️ Contrer une carapace bleue",
+                content: "3 façons de contrer une carapace bleue : Avec une autre carapace bleue maintenue à l’arrière, avec un fantôme 👻 ou une étoile. ⭐"
+            },
+            tip7: {
+                title: "🎯 Piéger ses adversaires",
+                content: "Placez un faux bloc juste derrière un vrai bloc pour qu’il soit détecté au dernier moment."
+            },
+            tip8: {
+                title: "⚡ Maximiser les éclairs",
+                content: "Sur les courses avec des tremplins (Wario Stadium, Autodrome Royal, Château de Bowser, Jungle DK), lancez l’éclair lorsque les concurrents approchent du tremplin. Ils ne pourront pas le passer. Sur les courses avec de longs sauts secrets (Plage Koopa, Autodrome Royal, Route Arc-en-ciel), utilisez l’éclair au bon moment pour faire rater les passages secrets à vos adversaires. Enfin, sur Montagne Choco, utilisez le lors de la boucle pour faire tomber vos adversaires d'un niveau."
+            },
+            tip9: {
+                title: "🔊 Couper la musique",
+                content: "Pendant une partie, appuyez sur L pour adoucir ou couper la musique. Recommencez pour la réactiver."
+            },
+            tip10: {
+                title: "🏁 Voir les records de Mario Raceway",
+                content: "À l’écran titre, appuyez sur Z pour voir les meilleurs temps de Mario Raceway (mode Time Trial)."
+            }
+        },
+        beta: {
+            title: "Version Beta",
+            content: {
+                part1: "The game is currently in beta. We are working hard to make it perfect for you. ",
+                part2: "We are working on the multiplayer mode and the online mode. We are also working on the AI to make it more realistic.",
+                part3: "We are also working on the graphics and the sound. We are also working on the physics to make it more realistic."
+            }
+        },
+        footer: {
+            copyright: {
+                year: "2025 Les Allumés du Kart. © ",
+                company: "Culture Games",
+                rights: ". - Tous droits réservés.",
+                nintendo: "Les personnages et éléments des jeux de ce site sont les propriétés de la société Nintendo."
+            }
+        }
+    },
+    en: {
+        subnav: {
+            maps: "Map Secrets",
+            tips: "Tips",
+            beta: "Beta"
+        },
+        legend: {
+            show: "Show legend",
+            hide: "Hide legend",
+            difficulty: {
+                titleDifficulty: "Difficulty",
+                veryEasy: "Very easy",
+                easy: "Easy",
+                medium: "Medium",
+                hard: "Hard",
+                veryHard: "Very hard"
+            },
+            gain: {
+                titleGain: "Gain",
+                veryLittle: "A tiny portion",
+                little: "⅛ of a lap",
+                medium: "¼ of a lap",
+                aLot: "½ of a lap",
+                reallyALot: "1 lap"
+            },
+            mode: {
+                titleMode: "Modes",
+                time: "Time Trial",
+                GrandPrix: "Grand Prix",
+                Multiplayers: "Multiplayers",
+            }
+        },
+        size: {
+            decrease: "Decrease",
+            increase: "Increase"
+        },
+        circuitsData: circuitsDataEN,
+        tips: {
+            title: "Tips",
+            tip1: {
+                title: "🏎️ Red shell in first position",
+                content: "A red shell fired while in first position will travel around the track until it hits a player on the road. 🎯"
+            },
+            tip2: {
+                title: "🌬️ Drafting",
+                content: "Take advantage of drafting behind another player to gain speed and overtake easily. 🚀"
+            },
+            tip3: {
+                title: "🍌 Avoid slipping on a banana",
+                content: "When you hit a banana, quickly press the brake button to avoid losing control of your vehicle. A small musical note 🎵 will appear if done correctly!"
+            },
+            tip4: {
+                title: "🚦 Quick start",
+                content: "Press the accelerator a fraction of a second before the light turns green for a boost start."
+            },
+            tip5: {
+                title: "🏎️ Boosting out of a turn",
+                content: "Use the R button to drift through a turn. Wiggle the control stick left and right to change the smoke color (yellow → orange). Release R when it’s orange for a mini boost."
+            },
+            tip6: {
+                title: "🛡️ Blocking a blue shell",
+                content: "3 ways to block a blue shell: Hold another blue shell behind you, use a ghost 👻 or a star. ⭐"
+            },
+            tip7: {
+                title: "🎯 Tricking your opponents",
+                content: "Place a fake item box right behind a real one, making it hard to detect until the last moment."
+            },
+            tip8: {
+                title: "⚡ Maximizing lightning",
+                content: "In tracks with ramps (Wario Stadium, Royal Raceway, Bowser’s Castle, DK Jungle), use the lightning bolt just as opponents approach the ramp to stop them from jumping. In tracks with long shortcut jumps (Koopa Troopa Beach, Royal Raceway, Rainbow Road), use the lightning at the perfect moment to make opponents miss the shortcut. Finally, on Choco Mountain, use it during the loop to knock your opponents down a level."
+            },
+            tip9: {
+                title: "🔊 Turn off the music",
+                content: "During the game, press L to lower or turn off the music. Press again to bring it back."
+            },
+            tip10: {
+                title: "🏁 View Mario Raceway records",
+                content: "On the title screen, press Z to view the best times on Mario Raceway (Time Trial mode)."
+            }
+        },
+        beta: {
+            title: "Beta version",
+            content: {
+                part1: "The game is currently in beta. We are working hard to make it perfect for you. ",
+                part2: "We are working on the multiplayer mode and the online mode. We are also working on the AI to make it more realistic.",
+                part3: "We are also working on the graphics and the sound. We are also working on the physics to make it more realistic."
+            }
+        },
+        footer: {
+            copyright: {
+                year: "2025 The Kart Fanatics. © ",
+                company: "Culture Games",
+                rights: ". - All rights reserved.",
+                nintendo: "The characters and elements of the games on this site are the properties of the Nintendo company."
+            }
+        }
+    }
+}
